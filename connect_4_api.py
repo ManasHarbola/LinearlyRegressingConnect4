0875f6d6946deca533286ecdad7654b73ddb6902
mharbola3 <mharbola3@gatech.edu>
2021-10-23 16:14:45 -0400

    Add rating algo

--- connect_4_api.py
+++ connect_4_api.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import time
+import numpy as np
 
 class Connect_4_API:
     GET_URL = "https://connect4.gamesolver.org/solve?pos="
@@ -15,7 +15,8 @@ class Connect_4_API:
         "Sec-Fetch-Site": "none",
         "Sec-Fetch-User": "?1",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0"
+
     }
     #method to grab best possible moves
     #used for building the model of the user
@@ -26,33 +27,27 @@ class Connect_4_API:
     #returns tuple of normalized average optimality score and the std dev of optimal moves made by opponent
     def rateOppMoves(state):
         moveScores = []
-        for i in range(len(state)):
-            if i % 2 == 0:
-                print(state[:i+1])
-                r = requests.get(Connect_4_API.GET_URL + state[:i+1], headers=Connect_4_API.GET_HEADER)
-                print(r.text)
-                time.sleep(0.25)
+        for i in range(0, len(state), 2):
+            r = requests.get(Connect_4_API.GET_URL + state[:i], headers=Connect_4_API.GET_HEADER)
+            data = r.json()
+            colScores = data['score']
+            scoreFreq = {}
+            numChoices = 0
+            for score in colScores:
+                if score != 100:
+                    scoreFreq[score] = scoreFreq.get(score, 0) + 1
+                    numChoices += 1
 
-    #send move made by AI based on player simulation
-    #def postNextMove(move):
-        #blah
+            scores = [(k, scoreFreq[k]) for k in reversed(scoreFreq.keys())]
+            moveScore = colScores[int(state[i]) - 1]
+            movesBetterThan = numChoices
 
-'''
-for i in range(100):
-    r = requests.get("https://connect4.gamesolver.org/solve?pos=1", headers=headers)
-    print(r.text)
-'''
-#data = r.json()
-#jsonStr = json.dumps(data)
-#print(jsonStr)
+            for i in range(len(scores)):
+                if scores[i][0] > moveScore:
+                    movesBetterThan -= scores[i][1]
+                else:
+                    moveScores.append(movesBetterThan / numChoices)
 
+        return (np.mean(moveScores), np.std(moveScores))
 
-s = "444447123311215666611673322237657452355775"
-for i in range(len(s)):
-    print(s[:i+1])
-    r = requests.get((Connect_4_API.GET_URL + s[:i+1]), headers=Connect_4_API.GET_HEADER)
-    data = r.json()
-    print(data.get("score"))
-    time.sleep(1)
-
-#Connect_4_API.rateOppMoves("444447123311215666611673322237657452355775")
+#print(Connect_4_API.rateOppMoves("444447123311215666611673322237657452355775"))
