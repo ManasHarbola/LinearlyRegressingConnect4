f434ed20c60c8f909f414fdb2cdf9c61179b8f56
mharbola3 <mharbola3@gatech.edu>
2021-10-23 21:24:54 -0400

    add ranked leaderboard feature

--- main.py
+++ main.py
@@ -169,43 +169,17 @@ class Connect_4_API:
         print(found_user)
 
         return {"mean": str(np.mean(moveScores)), "std": str(np.std(moveScores))}
-      
-    #send move made by AI based on player simulation
-    #def postNextMove(move):
-        #blah
 
-"""
-print("hello")
-api = Connect_4_API()
-
-GET_HEADER = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Connection": "keep-alive",
-        "Host": "connect4.gamesolver.org",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0"
-}
-r = requests.get("https://connect4.gamesolver.org/solve?pos=1", headers=GET_HEADER)
-print(r.text)
-api.testGetReq("444447123311")
-
-"""
-
-'''
-for i in range(100):
-    r = requests.get("https://connect4.gamesolver.org/solve?pos=1", headers=headers)
-    print(r.text)
-'''
-#data = r.json()
-#jsonStr = json.dumps(data)
-#print(jsonStr)
-#print(Connect_4_API.rateOppMoves("44444723333213216275"))
+    def getArenaLeaderboard(arenaID):
+        try:
+            userList = LeaderBoards.query.filter_by(arena_id = arenaID).first()
+            ranked = []
+            for user in LeaderBoards.users:
+                ranked.append((user.id, user.average, user.standardDev))
+            ranked.sort(key=lambda user : (-user[1], user[2]))
+            return [user[0] for user in ranked]
+        except:
+            return "FAILURE"
 
 db.create_all()
 
